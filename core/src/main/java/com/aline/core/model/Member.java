3e75e49b82284723df7f84cbe44b3947d6b6421c
Jorge Luis Canales Jr. <canalesjl31@gmail.com>
2024-03-29 11:28:29 -0500

    Modified the member file to include setters and getters

--- core/src/main/java/com/aline/core/model/Member.java
+++ core/src/main/java/com/aline/core/model/Member.java
@@ -2,13 +2,9 @@ package com.aline.core.model;
 
 import com.aline.core.listener.CreateMemberListener;
 import com.aline.core.model.account.Account;
-import com.aline.core.model.card.Card;
+import com.aline.core.model.account.Card;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -35,7 +31,7 @@ public class Member {
     @OneToOne(optional = false)
     private Applicant applicant;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY) // Changed fetch type to lazy
     @JoinTable(
             name = "account_holder",
             joinColumns = @JoinColumn(name = "member_id"),
@@ -45,7 +41,7 @@ public class Member {
     @ToString.Exclude
     private Set<Account> accounts;
 
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY) // Changed fetch type to lazy
     @JoinTable(
             name = "card_holder",
             joinColumns = @JoinColumn(name = "member_id"),
@@ -55,4 +51,60 @@ public class Member {
     @ToString.Exclude
     private Set<Card> cards;
 
+    // Constructors
+    public Member(Branch branch, String membershipId, Applicant applicant, Set<Account> accounts) {
+        this.branch = branch;
+        this.membershipId = membershipId;
+        this.applicant = applicant;
+        this.accounts = accounts;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(Applicant applicant) {
+        this.applicant = applicant;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Set<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(Set<Card> cards) {
+        this.cards = cards;
+    }
 }
